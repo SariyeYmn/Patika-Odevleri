13f810a3bf476ff7787963836e0312a96f018bfd
SariyeYmn <101094825+SariyeYmn@users.noreply.github.com>
2023-02-01 12:06:15 +0300

    max-min eleman bulma
    patika ödev dizideki max min deger bulma

---
+++ DizideMaxMinDegerBulma/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class Main {
+	public static void main(String[] args) {
+		
+		int[] numbers = {12,13,7,24,2,25,30,40,10};
+		
+		Arrays.sort(numbers);
+		
+		System.out.println("Dizinin minumum degeri:"  + numbers[0]);
+		
+		System.out.println("Dizinin maksimun degeri:" + numbers[8]);
+		
+		
+	}
+
+}
